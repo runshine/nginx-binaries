3ce1d1f63acd84cde9e2ae42e4caa36a1b329797
runshine <woshiyuhao0819@gmail.com>
2025-08-29 04:42:36 +0000

    fixs

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+export CFLAGS="-Os -fomit-frame-pointer -pipe"
+export LINUX_LDFLAGS="-static -Wl,--as-needed -Wl,-Map,linker.map"
+export DARWIN_LDFLAGS="-Wl,-map,linker.map"
+export WIN32_LDFLAGS="-Wl,--as-needed -Wl,-Map,linker.map"
+export NGINX_MODULES="kjdev/nginx-auth-jwt kjdev/nginx-keyval vision5/ngx_devel_kit openresty/echo-nginx-module openresty/headers-more-nginx-module openresty/set-misc-nginx-module"
+  # The same as above, but for Windows.
+  # - kjdev/nginx-auth-jwt, kjdev/nginx-keyval: a bit complicated to build (TODO)
+export NGINX_MODULES_WIN32="vision5/ngx_devel_kit openresty/echo-nginx-module openresty/headers-more-nginx-module openresty/set-misc-nginx-module" # Don't update binaries with unchanged sources.
+export SKIP_SAME_SOURCES="true"
+
 apt-get update -qq && apt install -qq -y libedit-dev libncurses-dev libssl-dev libpcre2-dev libzstd-dev
 
 cd /build
