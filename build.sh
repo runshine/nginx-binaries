b08b1950488eca703400de8df98507fa0eebc805
runshine <woshiyuhao0819@gmail.com>
2025-08-29 04:44:34 +0000

    fixs

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ export NGINX_MODULES="kjdev/nginx-auth-jwt kjdev/nginx-keyval vision5/ngx_devel_
 export NGINX_MODULES_WIN32="vision5/ngx_devel_kit openresty/echo-nginx-module openresty/headers-more-nginx-module openresty/set-misc-nginx-module" # Don't update binaries with unchanged sources.
 export SKIP_SAME_SOURCES="true"
 
-apt-get update -qq && apt install -qq -y libedit-dev libncurses-dev libssl-dev libpcre2-dev libzstd-dev
+apt-get update -qq && apt install -qq -y libedit-dev libncurses-dev libssl-dev libpcre2-dev libzstd-dev libz-dev
 
 cd /build
 ls -lRt
